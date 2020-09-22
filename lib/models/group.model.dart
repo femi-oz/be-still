e2dc1a447f11d293326380302e517b3c4fe300ea
Segun Ajanaku <sajanaku@eminenttechnology.com>
2020-09-22 19:40:45 +0100

    update objects

--- lib/models/group.model.dart
+++ lib/models/group.model.dart
@@ -12,26 +12,11 @@ class GroupModel {
   final List<String> prayerList;
   final List<String> members;
   final List<String> moderators;
-  final String description;
   final String organization;
   final String status;
   final String location;
   final String createdBy;
 
-<<<<<<< HEAD:lib/src/Models/group.model.dart
-  const GroupModel(
-      {@required this.id,
-      @required this.name,
-      @required this.admin,
-      @required this.members,
-      @required this.prayerList,
-      @required this.moderators,
-      @required this.description,
-      @required this.organization,
-      @required this.status,
-      @required this.location,
-      @required this.createdBy});
-=======
   const GroupModel({
     @required this.id,
     @required this.name,
@@ -44,6 +29,9 @@ class GroupModel {
     @required this.members,
     @required this.prayerList,
     @required this.moderators,
+    @required this.organization,
+    @required this.status,
+    @required this.location,
+    @required this.createdBy,
   });
->>>>>>> 2a9c55534236874c9b1a8b5bdbae02139a004fcb:lib/models/group.model.dart
 }
