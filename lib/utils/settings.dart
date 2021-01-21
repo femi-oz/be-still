cb8a8c98314568403f958bac0bbddeafdd7ad51f
Segun Ajanaku <sajanaku@eminenttechnology.com>
2021-01-21 16:42:19 +0100

    disable light mode

--- lib/utils/settings.dart
+++ lib/utils/settings.dart
@@ -4,15 +4,24 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class Settings {
   static SharedPreferences sharedPrefs;
-  static Future init() async => sharedPrefs = await SharedPreferences.getInstance();
+  static Future init() async =>
+      sharedPrefs = await SharedPreferences.getInstance();
 
   static const String THEME_MODE_KEY = 'theme_mode';
 
-  static set themeMode(String mode) => sharedPrefs.setString(THEME_MODE_KEY, mode);
+  static set themeMode(String mode) =>
+      sharedPrefs.setString(THEME_MODE_KEY, mode);
 
-  static String get themeMode => sharedPrefs.getString(THEME_MODE_KEY) ?? BsThemeMode.light;
+  // static String get themeMode =>
+  //     sharedPrefs.getString(THEME_MODE_KEY) ?? BsThemeMode.light;
 
-  static bool isDarkMode = (sharedPrefs.getString(THEME_MODE_KEY) == BsThemeMode.auto
-      ? MediaQueryData.fromWindow(WidgetsBinding.instance.window).platformBrightness == Brightness.dark
-      : sharedPrefs.getString(THEME_MODE_KEY) == BsThemeMode.dark);
+  // static bool isDarkMode =
+  //     (sharedPrefs.getString(THEME_MODE_KEY) == BsThemeMode.auto
+  //         ? MediaQueryData.fromWindow(WidgetsBinding.instance.window)
+  //                 .platformBrightness ==
+  //             Brightness.dark
+  //         : sharedPrefs.getString(THEME_MODE_KEY) == BsThemeMode.dark);
+
+  static String get themeMode => BsThemeMode.dark;
+  static bool isDarkMode = true;
 }
