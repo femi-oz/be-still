6ca13d64ce76ecc4596a4b14803fb1cb529eb9ca
Segun Ajanaku <sajanaku@eminenttechnology.com>
2021-04-08 15:24:59 +0100

    fix bugs

--- lib/screens/prayer_details/widgets/update_view.dart
+++ lib/screens/prayer_details/widgets/update_view.dart
@@ -1,9 +1,14 @@
 import 'package:be_still/providers/prayer_provider.dart';
+import 'package:be_still/providers/user_provider.dart';
 
 import 'package:be_still/utils/essentials.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_sms/flutter_sms.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
+import 'package:easy_rich_text/easy_rich_text.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class UpdateView extends StatelessWidget {
   // final PrayerModel prayer;
@@ -13,6 +18,149 @@ class UpdateView extends StatelessWidget {
 
   @override
   UpdateView();
+  _emailLink([bool isChurch = false]) async {
+    final Uri params = Uri(scheme: 'mailto', path: '', query: "");
+
+    var url = params.toString();
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  _textLink([bool isChurch = false]) async {
+    String _result =
+        await sendSMS(message: '', recipients: []).catchError((onError) {
+      print(onError);
+    });
+    print(_result);
+
+    // final uri =
+    //     "sms:${isChurch ? _churchPhone : ''}${Platform.isIOS ? '&' : '?'}body=$_prayer \n\n ${updates != '' ? 'Comments  \n $updates \n\n' : ''}$_footerText";
+
+    // if (await canLaunch(uri)) {
+    //   await launch(uri);
+    // } else {
+    //   throw 'Could not launch $uri';
+    // }
+  }
+
+  _openShareModal(BuildContext context) {
+    AlertDialog dialog = AlertDialog(
+        actionsPadding: EdgeInsets.all(0),
+        contentPadding: EdgeInsets.all(0),
+        backgroundColor: AppColors.prayerCardBgColor,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(color: AppColors.darkBlue),
+          borderRadius: BorderRadius.all(
+            Radius.circular(10.0),
+          ),
+        ),
+        content: Container(
+          height: MediaQuery.of(context).size.height * 0.2,
+          child: Stack(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+                child: Container(
+                  margin: EdgeInsets.only(top: 5, right: 5),
+                  alignment: Alignment.topRight,
+                  child: Icon(
+                    Icons.cancel,
+                    color: AppColors.red,
+                    size: 20,
+                  ),
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 40),
+                      width: double.infinity,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          GestureDetector(
+                            onTap: () {
+                              _emailLink(false);
+                            },
+                            child: Container(
+                              height: 30,
+                              width: MediaQuery.of(context).size.width * .20,
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: AppColors.cardBorder,
+                                  width: 1,
+                                ),
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text(
+                                    'EMAIL',
+                                    style: TextStyle(
+                                      color: AppColors.lightBlue4,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              _textLink(false);
+                            },
+                            child: Container(
+                              height: 30,
+                              width: MediaQuery.of(context).size.width * .20,
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: AppColors.cardBorder,
+                                  width: 1,
+                                ),
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text(
+                                    'SMS',
+                                    style: TextStyle(
+                                      color: AppColors.lightBlue4,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return dialog;
+        });
+  }
+
   Widget build(BuildContext context) {
     final prayerData = Provider.of<PrayerProvider>(context).currentPrayer;
     final updates = prayerData.updates;
@@ -42,9 +190,15 @@ class UpdateView extends StatelessWidget {
                         .format(updates[i].modifiedOn)
                         .toLowerCase(),
                     updates[i].modifiedOn,
-                    updates[i].description),
-              _buildDetail('Initial Prayer Request |',
-                  prayerData.prayer.modifiedOn, prayerData.prayer.description),
+                    updates[i].description,
+                    prayerData.tags,
+                    context),
+              _buildDetail(
+                  'Initial Prayer Request |',
+                  prayerData.prayer.modifiedOn,
+                  prayerData.prayer.description,
+                  prayerData.tags,
+                  context),
             ],
           ),
         ),
@@ -52,7 +206,7 @@ class UpdateView extends StatelessWidget {
     );
   }
 
-  Widget _buildDetail(time, modifiedOn, description) {
+  Widget _buildDetail(time, modifiedOn, description, tags, context) {
     return Container(
       child: Column(
         children: <Widget>[
@@ -92,13 +246,31 @@ class UpdateView extends StatelessWidget {
               child: Row(
                 children: [
                   Flexible(
-                    child: Text(
+                    child: EasyRichText(
                       description,
-                      style: AppTextStyles.regularText18b.copyWith(
-                        color: AppColors.prayerTextColor,
-                      ),
-                      textAlign: TextAlign.left,
+                      defaultStyle: AppTextStyles.regularText15
+                          .copyWith(color: AppColors.prayerTextColor),
+                      patternList: [
+                        for (var i = 0; i < tags.length; i++)
+                          EasyRichTextPattern(
+                              targetString: tags[i].displayName,
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  _openShareModal(context);
+                                },
+                              style: AppTextStyles.regularText15.copyWith(
+                                  color: AppColors.lightBlue2,
+                                  decoration: TextDecoration.underline))
+                      ],
                     ),
+
+                    // Text(
+                    //   description,
+                    //   style: AppTextStyles.regularText18b.copyWith(
+                    //     color: AppColors.prayerTextColor,
+                    //   ),
+                    //   textAlign: TextAlign.left,
+                    // ),
                   ),
                 ],
               ),
