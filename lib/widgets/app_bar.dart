24d037c66082e160096f746937c3694434d93321
Babafemi Adojutelegan <badojutelegan@eminenttechnology.com>
2021-11-15 19:54:23 +0100

    fixed bugs

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -264,41 +264,52 @@ class _CustomAppBarState extends State<CustomAppBar> {
                     builder: (context) => NotificationsScreen(),
                   ),
                 ),
-                child: Stack(
-                  children: [
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      child: IconButton(
-                        icon: Icon(
-                            notifications.length != 0
-                                ? Icons.notifications
-                                : Icons.notifications_none,
-                            size: 30,
-                            color: notifications.length != 0
-                                ? AppColors.red
-                                : AppColors.white),
-                        onPressed: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => NotificationsScreen(),
+                child: Container(
+                  width: 40,
+                  child: Stack(
+                    children: [
+                      Align(
+                        alignment: Alignment.center,
+                        child: InkWell(
+                          child: Icon(
+                              notifications.length != 0
+                                  ? Icons.notifications
+                                  : Icons.notifications_none,
+                              size: 32,
+                              color: notifications.length != 0
+                                  ? AppColors.red
+                                  : AppColors.white),
+                          onTap: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => NotificationsScreen(),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    notifications.length != 0
-                        ? Positioned(
-                            top: MediaQuery.of(context).size.height * 0.037,
-                            left: notifications.length > 9
-                                ? MediaQuery.of(context).size.width * 0.044
-                                : MediaQuery.of(context).size.width * 0.053,
-                            child: Text(notifications.length.toString(),
-                                style: TextStyle(
-                                    fontSize: 10,
-                                    color: AppColors.white,
-                                    fontWeight: FontWeight.w600)),
-                          )
-                        : Container(),
-                  ],
+                      notifications.length != 0
+                          ? Align(
+                              alignment: Alignment.center,
+                              child: Container(
+                                padding: EdgeInsets.all(notifications.length > 9
+                                    ? 2
+                                    : notifications.length > 99
+                                        ? 1
+                                        : 3.8),
+                                width: 15,
+                                child: FittedBox(
+                                  fit: BoxFit.cover,
+                                  child: Text(notifications.length.toString(),
+                                      style: TextStyle(
+                                          fontSize: 10,
+                                          color: AppColors.white,
+                                          fontWeight: FontWeight.w600)),
+                                ),
+                              ),
+                            )
+                          : Container(),
+                    ],
+                  ),
                 ),
               )
             : Container(),
