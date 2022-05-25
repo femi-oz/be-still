4c3c18bdeb80b89f436e01b1a96de6c7ae1a22f9
Segun Ajanaku <sajanaku@eminenttechology.com>
2022-05-25 11:35:45 +0100

    fix bug

--- lib/widgets/join_group.dart
+++ lib/widgets/join_group.dart
@@ -238,7 +238,6 @@ class JoinGroup {
           .requestToJoinGroup(
               groupData.group?.id ?? '',
               '$userName has requested to join your group',
-              [],
               (adminData.devices ?? []).map((e) => e.token ?? '').toList(),
               adminData.groups ?? []);
 
