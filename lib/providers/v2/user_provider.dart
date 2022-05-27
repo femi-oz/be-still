8fc6afb6895232d96c0491996388bde24999afd0
Segun Ajanaku <sajanaku@eminenttechology.com>
2022-05-27 10:08:02 +0100

    update

--- lib/providers/v2/user_provider.dart
+++ lib/providers/v2/user_provider.dart
@@ -42,11 +42,11 @@ class UserProviderV2 with ChangeNotifier {
         await Provider.of<PrayerProviderV2>(Get.context!, listen: false)
             .setPrayers();
         // (event.prayers ?? []).map((e) => e.prayerId ?? '').toList()
-        // await Provider.of<GroupProviderV2>(Get.context!, listen: false)
-        //     .setUserGroups(event.groups ?? <String>[]);
-
         await Provider.of<GroupProviderV2>(Get.context!, listen: false)
-            .onGroupChanges(event.groups ?? <String>[]);
+            .setUserGroups(event.groups ?? <String>[]);
+
+        // await Provider.of<GroupProviderV2>(Get.context!, listen: false)
+        //     .onGroupChanges(event.groups ?? <String>[]);
 
         notifyListeners();
       });
