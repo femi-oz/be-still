93a9f176ac55131b752471483541fa2778c4bdaf
Segun Ajanaku <sajanaku@eminenttechology.com>
2022-07-06 13:22:46 +0100

    update

--- ios/appcenter-post-clone.sh
+++ ios/appcenter-post-clone.sh
@@ -20,7 +20,7 @@ git clone -b stable https://github.com/flutter/flutter.git
 export PATH=`pwd`/flutter/bin:$PATH
 
 flutter channel stable
-flutter doctor
+# flutter doctor
 flutter pub get
 
 echo "Installed flutter to `pwd`/flutter"
